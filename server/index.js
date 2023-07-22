1abb76cdb846c90661386511419163af7b998718
nadeeauliahq <129242513+nadeeauliahq@users.noreply.github.com>
2023-07-22 15:26:36 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://deploy-baru-deh.vercel.app"],
+    origin: [""],
     methods: ["POST", "GET"],
     credentials: true
   }
