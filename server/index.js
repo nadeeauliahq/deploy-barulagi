7831246100a3e3811f5f7c767364526021edb997
nadeeauliahq <129242513+nadeeauliahq@users.noreply.github.com>
2023-07-22 15:09:33 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://deploy-project-frontend.vercel.app/"],
+    origin: [""],
     methods: ["POST", "GET"],
     credentials: true
   }
